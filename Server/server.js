1f32a956a128c7eaf5d3c98e6744cd1ab09568d5
EduardoGonzalez4321 <62006769+EduardoGonzalez4321@users.noreply.github.com>
2023-11-30 22:28:26 -0500

    Add files via upload

---
+++ Server/server.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const path = require('path');
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const router = require('./controllers/router');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+//const port = 8080;
+
+// Configuración de CORS
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true, // Habilita el intercambio de cookies entre dominios
+  optionsSuccessStatus: 204,
+};
+
+const port = process.env.PORT || 8080;
+
+app.use(cors(corsOptions));
+
+// Middleware para parsear el cuerpo de las solicitudes como JSON
+app.use(express.json());
+
+// Directorio publico para el frontend
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+// Rutas
+app.use('/api/users', router);
+
+// Ruta de scripts
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
+
+/*
+app.get(['/', '/home'], (req, res) => {
+  res.sendFile(path.join(__dirname, 'home.html'));
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+})
+*/
+
+// Configuración de SSL
+const options = {
+  key: fs.readFileSync('certs/key.pem'),      // Reemplaza con la ruta a tu clave privada
+  cert: fs.readFileSync('certs/cert.pem'),    // Reemplaza con la ruta a tu certificado
+};
+
+// Crear servidor HTTPS
+//const server = https.createServer(options, app).listen(443);
+
+// Iniciar servidor
+app.listen(port, () => {
+  console.log(`EduMentor API listening on port ${port} with SSL.`);
+});
